578dc85c5ca7a235a2be71aab41243eb33c41e84
Coderner <shreyashukla680@gmail.com>
2025-08-05 23:08:34 +0530

    server urls and http methods

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req,res)=>{
-      const log = `${Date.now()} : New Request Received \n`;
+      if(req.url === "/favicon.ico")
+         return res.end();
+      const log = `${Date.now()} : method is ${req.method} url is ${req.url} \n`;
+      
+      const myUrl = url.parse(req.url, true)
+    //   console.log(myUrl); 
+
       fs.appendFile("log.txt",log,(err, data)=>{
-           res.end("Hello from server");
+           switch(myUrl.pathname){
+             case "/":
+                   qp=myUrl.query;
+                   res.end(`Welcome ${qp.name} to my main page\n`);
+                   break;
+              case "/about":
+                   res.end("This is about page of the website\n");
+                   break;
+              case "/query":
+                   res.end("query page\n");
+                   break;
+              default:
+                   res.end("404 Page not found!");
+           }
       });
 });
 
